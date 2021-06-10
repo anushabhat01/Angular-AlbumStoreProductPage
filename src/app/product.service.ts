3471814bb26c033fc724b5c2f99f417854b8a298
Anusha Bhat <anusha.bhat@compucom.com>
2021-06-10 12:45:23 +0530

    part 3

---
+++ src/app/product.service.ts
@@ -0,0 +1,9 @@
+import { Http, Response } from '@angular/http';
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http: Http) { }
+
+}
