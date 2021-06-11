eca226f4c6cfd63a6eea1a4cbecbced1b7a3f29d
Anusha Bhat <anusha.bhat@compucom.com>
2021-06-11 15:31:38 +0530

    part7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {RouterModule,Routes} from "@angular/router";
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
@@ -23,6 +23,7 @@ import { ProductListComponent } from './product-list/product-list.component';
     FormsModule,
     HttpModule
   ],
+  
   providers: [ProductService],
   bootstrap: [AppComponent]
 })
